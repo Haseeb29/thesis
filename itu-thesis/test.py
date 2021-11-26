7ad81f3f447b201fd67e97ecfc9d4f52ded1fde5
Haseeb29 <51755978+Haseeb29@users.noreply.github.com>
2021-11-26 19:24:39 +0500

    Update test.py

--- itu-thesis/test.py
+++ itu-thesis/test.py
@@ -4,3 +4,4 @@ import pickle, gzip
 import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
+gtd = pd.read_csv("GTD.csv",encoding = "ISO-8859-1")
