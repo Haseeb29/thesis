5de9e94d1b6973d18f26d1d36c27edca23d2a600
Haseeb29 <51755978+Haseeb29@users.noreply.github.com>
2021-11-26 19:13:58 +0500

    Update test.py

--- itu-thesis/test.py
+++ itu-thesis/test.py
@@ -1,1 +1,6 @@
-print("helloo")
+import pandas as pd
+import numpy as np
+import pickle, gzip
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
