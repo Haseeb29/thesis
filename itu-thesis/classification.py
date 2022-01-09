fbcd898a0554c1cec28c857d175934e2287ac53c
Haseeb29 <51755978+Haseeb29@users.noreply.github.com>
2022-01-10 00:51:38 +0500

    Update classification.py

--- itu-thesis/classification.py
+++ itu-thesis/classification.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle, gzip
 import matplotlib.pyplot as plt
 from sklearn import svm
-from sklearn.metrics import confusion_matrix, precision_recall_fscore_support, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
@@ -17,14 +17,33 @@ mergedDF_x = mergedDF_x.values
 min_max_scaler = MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(mergedDF_x)
 y = mergedDF['label'].values
-x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.25, random_state=0)
 model1 = svm.SVC(C=5, kernel='rbf')
-print("fitting")
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
-print(y_test.shape)
+print("SVM fitting")
 model1.fit(x_train, y_train)
 pred1 = model1.predict(x_test)
 acc1 = accuracy_score(y_test,pred1)*100
-print(acc1)
+metrics = precision_recall_fscore_support(y_test, pred1, average='micro')
+print("SVM Accuracy",acc1)
+print("SVM Precision",metrics[0])
+print("SVM Recall",metrics[1])
+print("SVM Fscore",metrics[2])
+microF1 = f1_score(y_true, y_pred, average='micro')
+print("SVM MicroF1",microF1)
+macroF1 = f1_score(y_true, y_pred, average='macro')
+print("SVM MacroF1",macroF1)
+
+model1 = svm.SVC(C=5, kernel='rbf')
+print("Logistic fitting")
+clf = LogisticRegression(random_state=0).fit(x_train, y_train)
+pred1 = clf.predict(x_test)
+acc2 = accuracy_score(y_test,pred1)*100
+metrics2 = precision_recall_fscore_support(y_test, pred1, average='micro')
+print("Logistic Accuracy",acc1)
+print("Logistic Precision",metrics[0])
+print("Logistic Recall",metrics[1])
+print("Logistic Fscore",metrics[2])
+microF1 = f1_score(y_true, y_pred, average='micro')
+print("Logistic MicroF1",microF1)
+macroF1 = f1_score(y_true, y_pred, average='macro')
+print("Logistic MacroF1",macroF1)
